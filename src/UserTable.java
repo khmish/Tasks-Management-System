5d0239ba5bf0771f86654de815502fdbf83c96bf
khmish <30586743+khmish@users.noreply.github.com>
2017-08-01 13:28:34 +0300

    add UsersTable

---
+++ src/UserTable.java
@@ -0,0 +1,41 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author khmish
+ */
+public class UserTable {
+    
+    public boolean Insert(Users user)
+    {
+        return false;
+    }
+    public boolean Delete(Users user)
+    {
+        return false;
+    }
+    public boolean Update(Users user)
+    {
+        return false;
+    }
+    public boolean UpdatePass(Users user)
+    {
+        return false;
+    }
+    public Users getUser(Users user)
+    {
+        return null;
+    }
+    public ArrayList getAllUser()
+    {
+        return null;
+    }
+    
+}
