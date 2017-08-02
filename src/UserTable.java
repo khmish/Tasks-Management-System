4fa326bddf8a29b9af821de365f64a00b8d576cc
khmish <30586743+khmish@users.noreply.github.com>
2017-08-02 08:27:03 +0300

    update UsersTable class

--- src/UserTable.java
+++ src/UserTable.java
@@ -1,5 +1,8 @@
 
 import java.util.ArrayList;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,46 +16,87 @@ import java.util.ArrayList;
  */
 public class UserTable {
 
-    private Connector conn;
-    
+    private Connector connector;
+    private PreparedStatement ps;
     // the construct is providing connection to DB
     public UserTable() {
-                            //user ,pass, url
-        conn= new Connector(""  , ""    , "");
-        if(conn.connectTo()){
+        //user ,pass, url
+        connector = new Connector("", "", "");
+        if (connector.connectTo()) {
             System.out.println("connected!");
         }
+
+    }
+
+    public boolean Insert(Users user) {
         
-    }
-    
-    
-    public boolean Insert(Users user)
-    {
+        try {//an example of update DB-------change the update statement to delete of insert
+            ps = connector.conn.prepareStatement("UPDATE Messages SET description = ?, author = ? WHERE id = ? AND seq_num = ?");
+            // set the preparedstatement parameters--- the following para is just an example NOT real DB
+            ps.setString(1, "description");
+            ps.setString(2, "author");
+            ps.setInt(3, 0);
+            ps.setInt(4, 0);
+
+            // call executeUpdate to execute our sql update statement
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return false;
     }
-    public boolean Delete(Users user)
-    {
+
+    public boolean Delete(Users user) {
+        try {//an example of update DB-------change the update statement to delete of insert
+            ps = connector.conn.prepareStatement("UPDATE Messages SET description = ?, author = ? WHERE id = ? AND seq_num = ?");
+            // set the preparedstatement parameters--- the following para is just an example NOT real DB
+            ps.setString(1, "description");
+            ps.setString(2, "author");
+            ps.setInt(3, 0);
+            ps.setInt(4, 0);
+
+            // call executeUpdate to execute our sql update statement
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return false;
     }
-    public boolean Update(Users user)
-    {
+
+    public boolean Update(Users user) {
+        try {//an example of update DB-------change the update statement to delete of insert
+            ps = connector.conn.prepareStatement("UPDATE Messages SET description = ?, author = ? WHERE id = ? AND seq_num = ?");
+            // set the preparedstatement parameters--- the following para is just an example NOT real DB
+            ps.setString(1, "description");
+            ps.setString(2, "author");
+            ps.setInt(3, 0);
+            ps.setInt(4, 0);
+
+            // call executeUpdate to execute our sql update statement
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return false;
     }
-    public boolean UpdatePass(Users user)
-    {
+
+    public boolean UpdatePass(Users user) {
         return false;
     }
-    public ArrayList Search(String criteria)
-    {
+
+    public ArrayList Search(String criteria) {
         return null;
     }
-    public Users getUser(Users user)
-    {
+
+    public Users getUser(Users user) {
         return null;
     }
-    public ArrayList getAllUser()
-    {
+
+    public ArrayList getAllUser() {
         return null;
     }
-    
+
 }
