15d769140cbbb2ac1cb50fca80b383f8e451b085
almakkis1 <s2007mk@gmail.com>
2017-08-06 04:03:06 +0300

    Creating Tool class

---
+++ src/Tool.java
@@ -0,0 +1,47 @@
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author salehalmakki
+ */
+public class Tool {
+    Tool(){
+    }
+    
+    public String getMAC_Address() {
+        String MAC_address = null;
+        InetAddress ip;
+	try {
+
+            ip = InetAddress.getLocalHost();
+            //System.out.println("Current IP address : " + ip.getHostAddress());
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+            byte[] mac = network.getHardwareAddress();
+            //System.out.print("Current MAC address : ");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                    sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
+            //System.out.println(sb.toString());
+            MAC_address =  sb.toString();
+
+	} catch (UnknownHostException e) {
+		e.printStackTrace();
+	} catch (SocketException e){
+		e.printStackTrace();
+	}
+        
+        return MAC_address;
+    } 
+    
+}
